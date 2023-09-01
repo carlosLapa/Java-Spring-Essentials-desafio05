336088c42b8c7bbd914844b66168409e84127a93
carlosLapa <carlosferreiralapa@gmail.com>
2023-09-01 11:07:52 +0100

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,12 +8,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
-    public OrderItemDTO(Long id, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long id, String name, Double price, Integer quantity, String imgUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -21,6 +23,7 @@ public class OrderItemDTO {
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
+        imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getId() {
@@ -37,6 +40,10 @@ public class OrderItemDTO {
 
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     // designasse como "get" para padronizar o acesso e a geração do JSON
